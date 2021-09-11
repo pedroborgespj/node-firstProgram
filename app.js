d33df05aa32246cb62b8fb7360690526741c2d30
Pedro Borges <pedroborgespj@gmail.com>
2021-09-11 10:29:21 -0300

    update packages / add package.json

--- app.js
+++ app.js
@@ -1,14 +1,23 @@
+// Incluindo uma biblioteca
 const http = require('http');
+const url = require('url');
+const queryString = require('querystring');
 
-const hostname = '127.0.0.1';
+// Definição de endereço / URL
+const hostname = '127.0.0.1'; // localhost
 const port = 3000;
 
+// Implementação da regra de negócio
 const server = http.createServer((req, res) => {
-  res.statusCode = 200;
-  res.setHeader('Content-Type', 'text/plain');
-  res.end('Hello World');
+
+
+
+    res.statusCode = 200;
+    res.setHeader('Content-Type', 'text/plain');
+    res.end('Hello World');
 });
 
+// Execução
 server.listen(port, hostname, () => {
-  console.log(`Server running at http://${hostname}:${port}/`);
+    console.log(`Server running at http://${hostname}:${port}/`);
 });
