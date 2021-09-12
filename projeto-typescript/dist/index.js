f1dbd3dbd9e027dff0649e28ccef3ceceb89b516
Pedro Borges <pedroborgespj@gmail.com>
2021-09-12 12:35:54 -0300

    createServer

--- projeto-typescript/dist/index.js
+++ projeto-typescript/dist/index.js
@@ -1,2 +1,9 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
+// Importação de bibliotecas
+var http_1 = require("http");
+//Definição de porta
+var port = 5000;
+var server = (0, http_1.createServer)(function (request, response) {
+});
+// Execução
--- projeto-typescript/index.ts
+++ projeto-typescript/index.ts
@@ -1,5 +1,14 @@
 // Importação de bibliotecas
-import * as http from 'http';
+import { createServer, IncomingMessage, ServerResponse } from 'http';
 import * as queryString from 'query-string';
 import * as url from 'url';
 import * as fs from 'fs';
+
+//Definição de porta
+const port = 5000;
+
+const server = createServer((request: IncomingMessage, response: ServerResponse) => {
+    
+})
+
+// Execução
