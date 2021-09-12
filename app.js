395f7f576a2bd0dab7ad5a75fa133c29a332c8be
Pedro Borges <pedroborgespj@gmail.com>
2021-09-12 11:09:44 -0300

    update selecionar / remover usuario

--- app.js
+++ app.js
@@ -11,20 +11,50 @@ const port = 3000;
 // Implementação da regra de negócio
 const server = http.createServer((req, res) => {
 
+    var resposta;
+    const urlparse = url.parse(req.url, true);
     // Receber informações do usuário
-    const params = queryString.parse(url.parse(req.url, true).search);
+    const params = queryString.parse(urlparse.search);
 
-    // Salvas as informações em um arquivo
-    fs.writeFile('users/mynewfile3.txt', 'Hello content!', function (err) {
-        if (err) throw err;
-        console.log('Saved!');
-    })
+    // Criar um usuario && Atualizar um usuario
+    if(urlparse.pathname == '/criar-usuario'){
 
+        // Salvas as informações em um arquivo
+        fs.writeFile('users/' + params.id + '.txt', JSON.stringify(params), function (err) {
+            if (err) throw err;
+            console.log('Saved!');
+        });
 
+        resposta = 'Usuario criado com sucesso';
 
-    res.statusCode = 200;
-    res.setHeader('Content-Type', 'text/plain');
-    res.end('Hello World');
+        res.statusCode = 200;
+        res.setHeader('Content-Type', 'text/plain');
+        res.end(resposta);
+    }
+    // Selecionar usuario
+    else if (urlparse.pathname == '/selecionar-usuario') {
+        fs.readFile('users/' + params.id + '.txt', function(err, data) {
+            resposta = data;
+
+            res.statusCode = 200;
+            res.setHeader('Content-Type', 'application/json');
+            res.end(resposta);
+        });
+    }
+    // Remover o usuario
+    else if (urlparse.pathname == '/remover-usuario') {
+        fs.unlink('users/' + params.id + '.txt', function (err) {
+            if (err) throw err;
+            console.log('File deleted!');
+
+            resposta = 'Usuario removido com sucesso';
+
+            res.statusCode = 200;
+            res.setHeader('Content-Type', 'text/plain');
+            res.end(resposta);
+        });
+    }
+
 });
 
 // Execução
